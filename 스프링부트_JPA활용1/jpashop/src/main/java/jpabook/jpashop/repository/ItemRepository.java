2a36a50ae79246cbe23334fb55d3b470f3c3bd2a
heewonham <huiwonham6@u.sogang.ac.kr>
2022-02-25 21:19:22 +0900

    inflearn - 스프링부트와 JPA활용1 : 예제

---
+++ 스프링부트_JPA활용1/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            // id 값이 없다는 것은 신규를 등록하는 것임
+            em.persist(item);
+        } else{
+            // update의 의미
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
