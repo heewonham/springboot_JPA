08f29572eaf5404d1cd575a91c726e9a569f6e74
heewonham <huiwonham6@u.sogang.ac.kr>
2022-02-27 20:59:47 +0900

    Spring MVC 1주차

---
+++ 스프링MVC_1/servlet/src/main/java/mvc/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,38 @@
+package mvc.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+        System.out.println("RequestParamServlet.service");
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        request.getParameterNames().asIterator()
+                        .forEachRemaining(paramName ->
+                                System.out.println(paramName + " = " + request.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println();
+        System.out.println("[단일 파라미터 조회] - end");
+
+        System.out.println("[이름이 같은 복수 파라미터 조회] - start");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
+        System.out.println("[이름이 같은 복수 파라미터 조회] - end");
+    }
+}
