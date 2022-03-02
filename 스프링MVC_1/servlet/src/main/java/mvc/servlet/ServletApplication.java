7bbdc72706593622922e472ada76da5adb309901
heewonham <huiwonham6@u.sogang.ac.kr>
2022-03-02 23:01:16 +0900

    Spring MVC - MVC 구조 이해

--- 스프링MVC_1/servlet/src/main/java/mvc/servlet/ServletApplication.java
+++ 스프링MVC_1/servlet/src/main/java/mvc/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package mvc.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +13,8 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+//	@Bean -> 자동으로 해줌
+//	InternalResourceViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 }
